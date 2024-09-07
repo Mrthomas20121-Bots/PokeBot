e6ce9bda54fe98a3da221aa34e73739d78176538
Thomas B <thomas77300@gmail.com>
2024-09-07 10:04:28 +0200

    foundation of the bot

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+import { Constants, Client, CommandInteraction } from 'eris';
 import Pokedex from 'pokedex-promise-v2';
+import { token } from './auth.js';
 
+// pokedex
 const pokedex = new Pokedex();
+
+const bot = new Client(token, {
+    autoreconnect: true
+});
+
+bot.on('ready', async () => {
+    console.log('bot is online');
+
+    bot.editStatus('online', {
+        name: `to the pokemon theme song`,
+        type: 2 // type '2' is listening to
+    });
+});
