8a5a14fe88f52c54ae37ad5a28af570e5353be26
Thomas B <thomas77300@gmail.com>
2024-09-07 12:56:56 +0200

    who let me cook seriously

--- index.js
+++ index.js
@@ -16,4 +16,58 @@ bot.on('ready', async () => {
         name: `to the pokemon theme song`,
         type: 2 // type '2' is listening to
     });
+
+    bot.createCommand({
+        name: 'ability',
+        description: 'Look up an ability',
+        options: [
+            {
+                name: 'list',
+                description: 'List all ability by name',
+                type: Constants.ApplicationCommandOptionTypes.STRING,
+                required: true,
+                choices: [
+                    {
+                        name: 'list',
+                        value: 'list'
+                    }
+                ]
+            }
+        ],
+        type: Constants.ApplicationCommandTypes.CHAT_INPUT
+    });
+
+    bot.createCommand({
+        name: 'abilitylist',
+        description: 'List all ability available',
+        type: Constants.ApplicationCommandTypes.CHAT_INPUT
+    });
 });
+
+bot.on("interactionCreate", async (interaction) => {
+    if(interaction instanceof CommandInteraction) {
+        let name = interaction.data.name;
+        if(name == 'abilitylist') {
+            pokedex.getAbilityByName('13')
+                .then(ability => {
+                    interaction.createMessage(ability.name);
+                }).catch(error => {
+                    console.log(error);
+                });
+        }
+        else if(name == 'abilitylist') {
+            let args = interaction.data.options;
+
+            if(args[0] == 'list') {
+                pokedex.getAbilityByName('13')
+                .then(ability => {
+                    interaction.createMessage(ability.name);
+                }).catch(error => {
+                    console.log(error);
+                });
+            }
+        }
+    }
+});
+
+bot.connect();
